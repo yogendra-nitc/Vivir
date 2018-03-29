810cdb7a235023a3790476f6916ab610b41b7ae4
yogendra-nitc <steptostep93@gmail.com>
2018-03-29 10:44:16 +0530

    Sliding view in Flat Details activity

--- app/src/main/java/com/example/yogendra/vivir/flatDetails.java
+++ app/src/main/java/com/example/yogendra/vivir/flatDetails.java
@@ -1,13 +1,21 @@
 package com.example.yogendra.vivir;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v4.view.ViewPager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class flatDetails extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flat_details);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
