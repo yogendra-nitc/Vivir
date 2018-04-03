de65b951d6d768596c208dabacf43cfba90bd3a3
yogendra-nitc <steptostep93@gmail.com>
2018-04-03 13:14:59 +0530

    User signup and Database connectivity

---
+++ app/src/main/java/com/example/yogendra/vivir/defConstant.java
@@ -0,0 +1,10 @@
+package com.example.yogendra.vivir;
+
+/**
+ * Created by yogendra on 3/4/18.
+ */
+
+public class defConstant {
+    public static final String ROOT_URL = "https://vivir18.000webhostapp.com/";
+    public  static  final String URL_SIGNUP = ROOT_URL + "userSignup.php";
+}
--- app/src/main/java/com/example/yogendra/vivir/signup.java
+++ app/src/main/java/com/example/yogendra/vivir/signup.java
@@ -1,5 +1,6 @@
 package com.example.yogendra.vivir;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,13 +8,47 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class signup extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class signup extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextFname, editTextLname,editTextEmail, editTextPassword;
+    private Button registerButton;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        editTextFname = (EditText)findViewById(R.id.fname);
+        editTextLname = (EditText)findViewById(R.id.lname);
+        editTextEmail = (EditText)findViewById(R.id.email);
+        editTextPassword = (EditText)findViewById(R.id.password);
+
+        registerButton = (Button)findViewById(R.id.email_register_btn);
+
+        progressDialog = new ProgressDialog(this);
+
+        registerButton.setOnClickListener(this);
+
+        // Alredy registered
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,5 +61,55 @@ public class signup extends AppCompatActivity {
                 startActivity(in);
             }
         });
+    }
+
+    public void registerUser(){
+        String fname = editTextFname.getText().toString().trim();
+        String lname = editTextLname.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String name = fname + lname;
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                defConstant.URL_SIGNUP,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.hide();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == registerButton){
+            registerUser();
+        }
     }
 }
