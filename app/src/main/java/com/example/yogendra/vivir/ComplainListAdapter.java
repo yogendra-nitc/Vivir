a0ce690bdb5fcc1e492f90c9b5d81db5a36fdf37
yogendra-nitc <steptostep93@gmail.com>
2018-04-04 23:25:21 +0530

    Complaint List RecylerView

--- app/src/main/java/com/example/yogendra/vivir/ComplainListAdapter.java
+++ app/src/main/java/com/example/yogendra/vivir/ComplainListAdapter.java
@@ -1,4 +1,5 @@
 package com.example.yogendra.vivir;
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,60 +12,59 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-/** * Created by yogenddra on 29/03/2018. */
+/**
+ * Created by yogendra on 4/4/18.
+ */
 
-public class SearchActivityAdapter  extends
-        RecyclerView.Adapter<SearchActivityAdapter.Holderview>{
+public class ComplainListAdapter  extends
+        RecyclerView.Adapter<ComplainListAdapter.Holderview>{
 
-    private List<SearchItem> flatList;
+    private List<ComplaintValue> complaintList;
     private Context context;
 
-    public SearchActivityAdapter(List<SearchItem> flatList, Context context) {
-        this.flatList = flatList;
+    public ComplainListAdapter(List<ComplaintValue> complaintList, Context context) {
+        this.complaintList = complaintList;
         this.context = context;
     }
 
     @Override
     public Holderview onCreateViewHolder(ViewGroup parent, int viewType) {
         View layout= LayoutInflater.from(parent.getContext()).
-                inflate(R.layout.search_custom_layout, parent,false);
+                inflate(R.layout.custom_activity_complain_list, parent,false);
 
         return new Holderview(layout);
     }
 
     @Override    public void onBindViewHolder(Holderview holder, final int position) {
 
-        holder.v_name.setText(flatList.get(position).getName());
-        holder.v_image.setImageResource(flatList.get(position).getPicture());
+        holder.v_name.setText(complaintList.get(position).getName());
+        holder.v_image.setImageResource(complaintList.get(position).getPicture());
+        holder.v_date.setText(complaintList.get(position).getCdate());
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override            public void onClick(View view) {
-                Toast.makeText(context, "click on " + flatList.get(position).getName(),
+                Toast.makeText(context, "click on " + complaintList.get(position).getName(),
                         Toast.LENGTH_LONG).show();
             }
         });
     }
 
     @Override    public int getItemCount() {
-        return flatList.size();
+        return complaintList.size();
     }
 
-    public void setfilter(List<SearchItem> listitem)
-    {
-        flatList=new ArrayList<>();
-        flatList.addAll(listitem);
-        notifyDataSetChanged();
-    }
     class Holderview extends RecyclerView.ViewHolder
     {
         ImageView v_image;
         TextView v_name;
+        TextView v_date;
 
         Holderview(View itemview)
         {
             super(itemview);
-            v_image=(ImageView) itemView.findViewById(R.id.flatImage);
-            v_name = (TextView) itemView.findViewById(R.id.flatTitle);
+            v_image=(ImageView) itemView.findViewById(R.id.userImage);
+            v_name = (TextView) itemView.findViewById(R.id.userName);
+            v_date = (TextView) itemView.findViewById(R.id.cdate);
 
         }
     }
