547233cc1a428386a5785dcc12e0c7810e70443b
Yogendra <steptostep93@gmail.com>
2018-04-07 16:22:29 +0530

    method for login

--- Server Files/DbOperations.php
+++ Server Files/DbOperations.php
@@ -3,7 +3,7 @@ include('Database.php');
     class userStart extends Database{
         
         // user signup
-        public function userRegistration($name, $email,$password,$userType){
+        public function userRegistration($name, $email,$password){
             $sql = "SELECT * FROM user WHERE email = '$email'";
             $result = $this->connect()->query($sql);
             $numRows = $result->num_rows;
@@ -13,31 +13,9 @@ include('Database.php');
             }
             else
             {
-                $sql = "INSERT INTO user(email, password,name,userType)VALUES('$email','$password','$name','$userType')";
+                $rdate = "2018-04-06";
+                $sql = "INSERT INTO user(email, password,rdate)VALUES('$email','$password','$rdate')";
                 $result = $this->connect()->query($sql);
-                
-                // id generation
-                $i=0; $temp="v";
-                while($email[$i]!='@')
-                {
-                    $temp = $temp.$email[$i];
-                    $i = $i + 1;
-                }
-                echo "$temp";
-                if($userType == "tenant")
-                {
-                    $rdate = date("Y-m-d");
-                    $tid = $temp."t";
-                    $sql = "INSERT INTO tenant(tid,email,rdate)VALUES('$tid','$email','$rdate')";
-                    $result = $this->connect()->query($sql);
-                }
-                else if($userType == "owner")
-                {
-                    $rdate = date("Y-m-d");
-                    $oid = $temp."o";
-                    $sql = "INSERT INTO owner(oid,email,rdate)VALUES('$oid','$email','$rdate')";
-                    $result = $this->connect()->query($sql);
-                }
                 return 1;
             }
         }
@@ -45,13 +23,41 @@ include('Database.php');
         // user login
         public function userLogin($email, $password)
         {
-            $sql = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
+            $sql = "SELECT email, password FROM user WHERE email = '$email'";
             $result = $this->connect()->query()($sql);
             $numRows = $result->num_rows;
+
+            // CHECKING FOR REGISTRATION
+
             if(numRows>0)
             {
-                $sql = "SELECT * FROM tenant where email = '$email'";
+                $row = $result->fetch_array(MYSQLI_BOTH);
+                $DbEmail = $row['email'];
+                $DbPassword = $row['password'];
+
+                // CHECKING FOR WRONG PASSWORD
+
+                if(($DbEmail == $email) and ($DbPassword == $password))
+                    return 2;
+                else
+                    return 1;
             }
+            else
+            {
+                return 0;
+            }
+        }
+
+        // getUser Data after login
+        public function getAuthUserData($email)
+        {
+            $sql = "SELECT * FROM user where email = '$email'";
+            $result = $this->connect()->query()($sql);
+            while($row = $result->fetch_assoc())
+            {
+                $data[] = $row; 
+            }
+            return $data;
         }
     }
 
