a4f5b636221df2fd5f2e13a6d1a726f07a6bc4b3
Yogendra <steptostep93@gmail.com>
2018-04-13 00:30:34 +0530

    fetch all data from Apartment and image

--- Server Files/Booking.php
+++ Server Files/Booking.php
@@ -7,7 +7,8 @@ include('Database.php');
 
         public function Apartments()
         {
-        	$sql = "SELECT A.aptId,A.aptName,A.locality,A.city FROM apartment as A,image as I where A.aptId = I.aptId";
+        //	$sql = "SELECT A.aptId,A.aptName,A.locality,A.city,I.img1 FROM apartment as A,image as I where A.aptId = I.aptId";
+        $sql = "SELECT * FROM apartment as A,image as I where A.aptId = I.aptId";
         	$result = $this->connect()->query($sql);
         	$numRows = $result->num_rows;
           return $result;
--- Server Files/getAllFlat.php
+++ Server Files/getAllFlat.php
@@ -8,7 +8,7 @@
     	
     while($row = $result->fetch_array(MYSQLI_BOTH))
     {
-    	array_push($response,array("aptId"=>$row['aptId'],"aptName"=>$row['aptName'],"locality"=>$row['locality'],"city"=>$row['city'],"img1"=>$row['img1']));
+    	array_push($response,array("aptId"=>$row['aptId'],"aptName"=>$row['aptName'],"locality"=>$row['locality'],"city"=>$row['city'],"ownerid"=>$row['ownerid'],"aptType"=>$row['aptType'],"rentAmt"=>$row['rentAmt'],"img1"=>$row['img1'],"img2"=>$row['img2'],"img3"=>$row['img3']));
     }
     echo json_encode(array("AllFlat"=>$response));
 
