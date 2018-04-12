96104e614b08183fe17ef05fcc3992818265491c
Yogendra <steptostep93@gmail.com>
2018-04-13 00:19:54 +0530

    All flat listing

---
+++ Server Files/Booking.php
@@ -0,0 +1,16 @@
+<?php
+
+include('Database.php');
+    class Booking extends Database{
+        
+// ********** METHOD FOR FLAT BOOKING **********
+
+        public function Apartments()
+        {
+        	$sql = "SELECT A.aptId,A.aptName,A.locality,A.city FROM apartment as A,image as I where A.aptId = I.aptId";
+        	$result = $this->connect()->query($sql);
+        	$numRows = $result->num_rows;
+          return $result;
+        }
+    }
+?>
